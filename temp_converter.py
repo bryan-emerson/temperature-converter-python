bbf598bfcf494ec79aa08511655e7f4ec842fb90
Ali Spittel <ali07cat07@gmail.com>
2018-04-10 15:46:55 -0400

    add kelvin

--- temp_converter.py
+++ temp_converter.py
@@ -2,10 +2,19 @@ temp_unit = input("What is the temperature unit? ")
 degrees = int(input("What is the temperature to convert? "))
 
 if temp_unit == "f":
-    converted_temp = (degrees - 32) / 1.8
-    print(converted_temp)
+    celsius_temp = (degrees - 32) / 1.8
+    kelvin_temp = (degrees + 459.67) / 1.8
+    print("Celsius: ", celsius_temp)
+    print("kelvin: ", kelvin_temp)
 elif temp_unit == "C":
-    converted_temp = degrees * 1.8 + 32
-    print(converted_temp)
+    farenheit_temp = degrees * 1.8 + 32
+    kelvin_temp = degrees + 273.15
+    print("Fahrenheit: ", farenheit_temp)
+    print("kelvin: ", kelvin_temp)
+elif temp_unit == "k":
+    celsius_temp = degrees - 273.15
+    farenheit_temp = degrees * 1.8 - 459.67
+    print("Celsius: ", celsius_temp)
+    print("Fahrenheit: ", farenheit_temp)
 else:
     print("We haven't implemented that unit yet!")
